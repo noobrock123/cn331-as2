9c7aae2ac64756a711e60e760ffd841bea7ddb10
noobrock123 <garfiwgame@hotmail.com>
2022-09-12 13:40:24 +0700

    Some migration n' Stuff

--- student_quota/settings.py
+++ student_quota/settings.py
@@ -125,5 +125,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-	'http://127.0.0.1:8000/'
+	'http://127.0.0.1:8000/',
+    'http://127.0.0.1:8000/user_page/quota_request/',
 ]
