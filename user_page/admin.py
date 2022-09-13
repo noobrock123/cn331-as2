58ac85b68f0d33380095d0ec54474adc22b4b498
noobrock123 <garfiwgame@hotmail.com>
2022-09-13 22:38:46 +0700

    Add Year class to be created and set by admin

--- user_page/admin.py
+++ user_page/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Subject
+from .models import Subject, Year
 # Register your models here.
+class SubjectAdmin(admin.ModelAdmin):
+    filter_horizontal = ['subjects']
+
 admin.site.register(Subject)
-#admin.site.register(Student)
+admin.site.register(Year, SubjectAdmin)
